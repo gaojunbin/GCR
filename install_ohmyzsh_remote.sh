cd69588b59e3562ee66bdd2f23a4b4e29b593620
junbingao <gao.junbin.cn@gmail.com>
2024-03-06 13:48:03 +0800

    support no sudo permission user to install GCR

--- install_ohmyzsh_remote.sh
+++ install_ohmyzsh_remote.sh
@@ -1,6 +1,11 @@
 git clone https://github.com/gaojunbin/GCR.git ~/GCR
 cd ~/GCR
 
+# is root?
+echo -e "Are you in the root group? ([y]/n)? \c"
+read root_yon
+root_yon=${root_yon:-y}
+
 # uninstall old version
 echo -e "Uninstall old version? ([y]/n)? \c"
 
@@ -29,7 +34,18 @@ read yon
 yon=${yon:-y}
 
 if [ "${yon}" = "y" ];then
-    sudo apt install zsh -y
+    if [ "${root_yon}" = "y" ];then
+        sudo apt install zsh -y
+    else
+        cd
+        wget -O zsh.tar.xz https://sourceforge.net/projects/zsh/files/latest/download --no-check-certificate
+        mkdir zsh && unxz zsh.tar.xz && tar -xvf zsh.tar -C zsh --strip-components 1
+        cd zsh
+        ./configure --prefix=$HOME/zsh
+        make && make install
+        echo 'export PATH=$HOME/zsh/bin:$PATH' >> ~/.bash_profile
+        cd ~/GCR
+    fi
 fi
 
 # add to .zshrc file
@@ -58,18 +74,13 @@ read yon
 yon=${yon:-y}
 
 if [ "${yon}" = "y" ];then
-    sudo chsh -s $(which zsh) $(whoami)
-fi
-
-# add start zsh to bashrc
-echo -e "Add starting zsh in .bashrc? (y/[n])? \c"
-
-read yon
-yon=${yon:-n}
-
-if [ "${yon}" = "y" ];then
-    echo '# [Added by install_ohmyzsh_remote.ssh] starting zsh when starting.' >> ~/.bashrc
-    echo 'zsh' >> ~/.bashrc
+    if [ "${root_yon}" = "y" ];then
+        sudo chsh -s $(which zsh) $(whoami)
+    else
+        echo '# [Added by install_ohmyzsh_remote.ssh] starting zsh when starting.' >> ~/.bash_profile
+        echo 'export SHELL=$HOME/zsh/bin/zsh' >> ~/.bash_profile
+        echo 'exec $HOME/zsh/bin/zsh -l' >> ~/.bash_profile
+    fi
 fi
 
 echo -e "Installing for ohmyzsh complete! Now you can resart shell."
